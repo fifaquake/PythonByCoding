2db45959b872f0af678e0e0009e418821f45b505
Peng@Home <lipengweb@gmail.com>
2015-12-16 22:17:06 +0800

    add ch02_01.py

---
+++ ch02_01.py
@@ -0,0 +1,20 @@
+num = 2
+area = 58.7
+city = 'Berlin'
+country = "Germany"
+
+z = 10 + 5j
+counter = None
+index = None
+
+global var_global
+var_global = 10
+
+print(num)
+print(area)
+print(city)
+print(country)
+print(z)
+print(z.real)
+print(z.imag)
+print(var_global)
