353853341b5f87ef693debbd1273b7a6ec099713
Peng@Home <lipengweb@gmail.com>
2015-12-27 10:36:45 +0800

    Add part samples of Chapter11

---
+++ ch11_02.py
@@ -0,0 +1,29 @@
+import time
+import threading
+
+
+class MyThread(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.running = False
+
+    def run(self):
+        counter = 0
+        self.running = True
+        while self.running:
+            print('counter: ', str(counter))
+            time.sleep(2)
+            counter += 1
+
+    def stop(self):
+        print('stopping thread...')
+        self.running = False
+        self.join(2)
+
+my_thread = MyThread()
+my_thread.setDaemon(True)
+my_thread.start()
+
+input("Press Enter to Stop")
+my_thread.stop()
